06386714ef110d048f36064b75813d068ec9bb23
Saoni Mukherjee <sayani.mookherjee@gmail.com>
2015-08-06 10:43:13 -0400

    fixed the timers

--- timing/rsac.c
+++ timing/rsac.c
@@ -4,19 +4,25 @@
 #include<string.h>
 #include<time.h>
 
+#define BILLION 1000000000L
+
 void mod(int,int,int);
 
-void main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	int e = strtol(argv[1],NULL,0);
 	int n = strtol(argv[2],NULL,0);
 	int data = strtol(argv[3],NULL,2);
-	clock_t begin, end;
 
-	begin = clock();
+	uint64_t diff;
+	struct timespec start, end;
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	mod(data, e, n);
-	end = clock();
+	clock_gettime(CLOCK_MONOTONIC, &end);
 
-	printf("%d,%f\n", (int)(end - begin), (double)(end - begin) / CLOCKS_PER_SEC);
+	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+	return 0;
 }
 
 void mod(int base, int exponent, int den)
